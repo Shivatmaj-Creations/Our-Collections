b9b8df3c94d472de79a0605f07286ad8a6f2c7b0
Shivatmaj-Creations <khushlihankarxf@gmail.com>
2026-07-31 23:40:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const PRODUCTS = [
     description:
       "A serene, seated Bappa finished in warm earthen tones. Compact enough for the home altar, detailed enough to feel truly special.",
     images: [
-      "images/1.1.webp",
+      "images/1.1.jpeg",
       "images/1.2.jpeg",
       "images/1.3.jpeg",
     ],
